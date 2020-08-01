61001cb26b556e9f67bbd42648ff6f59582795c1
fernandoyto <fernando.toshioyto@gmail.com>
2020-08-01 10:41:30 -0300

    chapter 3 done

--- chapter3/vowels7.py
+++ chapter3/vowels7.py
@@ -1,11 +1,7 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
+vowels = {'a', 'e', 'i', 'o', 'u'}
 word = input('Provide a word to search for vowels: ')
-found = []
 
-for letter in word:
-    if letter in vowels:
-        if letter not in found:
-            found.append(letter)
+found = vowels.intersection(set(word))
 
 for vowel in found:
     print(vowel)
